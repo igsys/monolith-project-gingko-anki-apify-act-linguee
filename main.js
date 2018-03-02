9ad6f487098c8650a143dcd7f0666fcb2959ab8a
Michael Chan <michael@innova-global.com>
2018-03-02 16:02:10 +0900

    modified script

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const Apify = require('apify');
 const request = require('request-promise');
 const cheerio = require('cheerio');
 
+const results = [];
+
 Apify.main(async () => {
     // Get input of your act
     const input = await Apify.getValue('INPUT');
@@ -15,10 +17,30 @@ Apify.main(async () => {
     const html = await request(`https://www.linguee.com/${input.dictionary}/search?source=${input.source}&query=${input.query}`);
     const $ = cheerio.load(html)
 
+
+    // get meaning and examples:
+    $('.sortablemg.featured').each(function () {
+        let result = {}
+        result['meaning'] = $(this).find('.dictLink.featured').text().trim();
+        result['grammar'] = $(this).find('.tag_type').text().trim();
+        result.examples = [];
+
+        $(this).find('.example_lines').each(function (i) {
+            result.examples.push({
+                index: i,
+                mono: $(this).find('.tag_e').eq(i).find('.tag_s').text().trim(),
+                trans: $(this).find('.tag_e').eq(i).find('.tag_t').text().trim()
+            });
+        });
+
+        results.push(result);
+    });
+
     // And then save output
     const output = {
         crawledAt: new Date(),
-        dictionary: $('.dict_headline_for_0').text().trim()
+        results: results,
+        dictionary: $('.dict_headline_for_0').text().trim(),
     };
     console.log('My output:');
     console.dir(output);

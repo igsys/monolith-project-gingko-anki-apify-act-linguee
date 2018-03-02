bd67fc103410276e29d4630719e8fbadd25352f6
Michael Chan <michael@innova-global.com>
2018-03-02 16:33:27 +0900

    modified script

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const Apify = require('apify');
-const request = require('request-promise');
+const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 
 const results = [];
@@ -13,10 +13,14 @@ Apify.main(async () => {
     console.log('My input:');
     console.dir(input);
 
-    // Do something useful here
-    const html = await request(`https://www.linguee.com/${input.dictionary}/search?source=${input.source}&query=${input.query}`);
-    const $ = cheerio.load(html)
+    // Navigate to page
+    const uri = `https://www.linguee.com/${input.dictionary}/search?source=${input.source}&query=${input.query}`;
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(uri)
 
+    let html = await page.content()
+    const $ = cheerio.load(html)
 
     // get meaning and examples:
     $('.sortablemg.featured').each(function () {
@@ -26,21 +30,22 @@ Apify.main(async () => {
         result.examples = [];
 
         $(this).find('.example_lines').each(function (i) {
+            console.log($(this).find('.tag_e').eq(i).find('.tag_s').text().trim())
             result.examples.push({
                 index: i,
                 mono: $(this).find('.tag_e').eq(i).find('.tag_s').text().trim(),
                 trans: $(this).find('.tag_e').eq(i).find('.tag_t').text().trim()
             });
         });
-
         results.push(result);
     });
 
     // And then save output
     const output = {
         crawledAt: new Date(),
+        keyword: query,
         results: results,
-        dictionary: $('.dict_headline_for_0').text().trim(),
+        // html
     };
     console.log('My output:');
     console.dir(output);

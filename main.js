349ca8918b0204c851281b9bc730a078a6478e84
Michael Chan <michael@innova-global.com>
2018-03-12 18:30:57 +0900

    Build - latest revision | next: continue

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-const Apify = require('apify');
-const typeCheck = require('type-check').typeCheck;
-const puppeteer = require('puppeteer');
-const cheerio = require('cheerio');
+const Apify = require('apify')
+const typeCheck = require('type-check').typeCheck
+const puppeteer = require('puppeteer')
+const cheerio = require('cheerio')
 
-const results = [];
+const results = []
 
 // Definition of the input
 const INPUT_TYPE = `{
-    query: String,
-    source: String,
+    query: String
+    source: String
     translation: String
-}`;
+}`
 
 const LEVEL_TYPE = {
     NOVOICE: 'NOVOICE',
@@ -18,31 +18,30 @@ const LEVEL_TYPE = {
     EXPERT: 'EXPERT'
 }
 
-
 Apify.main(async () => {
     // Get input of your act
-    const input = await Apify.getValue('INPUT');
+    const input = await Apify.getValue('INPUT')
 
     // Fetch the input and check it has a valid format
     // You don't need to check the input, but it's a good practice.
     if (!typeCheck(INPUT_TYPE, input)) {
-        console.log('Expected input:');
-        console.log(INPUT_TYPE);
-        console.log('Received input:');
-        console.dir(input);
-        throw new Error('Received invalid input');
+        console.log('Expected input:')
+        console.log(INPUT_TYPE)
+        console.log('Received input:')
+        console.dir(input)
+        throw new Error('Received invalid input')
     }
 
-    console.log('My input:');
-    console.dir(input);
+    console.log('My input:')
+    console.dir(input)
 
     // Environment variables
-    const launchPuppeteer = process.env.NODE_ENV === 'development' ? puppeteer.launch : Apify.launchPuppeteer;
+    const launchPuppeteer = process.env.NODE_ENV === 'development' ? puppeteer.launch : Apify.launchPuppeteer
 
     // Navigate to page
-    const uri = `https://www.linguee.com/${input.translation}-${input.source}/search?source=${input.source}&query=${input.query}`;
-    const browser = await launchPuppeteer();
-    const page = await browser.newPage();
+    const uri = `https://www.linguee.com/${input.translation}-${input.source}/search?source=${input.source}&query=${input.query}`
+    const browser = await launchPuppeteer()
+    const page = await browser.newPage()
     await page.goto(uri)
 
     let html = await page.content()
@@ -51,9 +50,9 @@ Apify.main(async () => {
     // get meaning and examples
     $('.sortablemg.featured').each((i, element1) => {
         let result = {}
-        result['meaning'] = $(element1).find('.dictLink.featured').text().trim();
-        result['grammar'] = $(element1).find('.tag_type').text().trim();
-        result.examples = [];
+        result['meaning'] = $(element1).find('.dictLink.featured').text().trim()
+        result['grammar'] = $(element1).find('.tag_type').text().trim()
+        result.examples = []
 
         $(element1).find('.example_lines .example').each((j, element2) => {
             console.log(j, $(element2).find('.tag_s').text().trim())
@@ -64,11 +63,11 @@ Apify.main(async () => {
                     level: LEVEL_TYPE.INTERMEDIATE,
                     mono: $(element2).find('.tag_s').text().trim(),
                     tran: $(element2).find('.tag_t').text().trim()
-                });
+                })
             }
-        });
-        results.push(result);
-    });
+        })
+        results.push(result)
+    })
 
     // And then save output
     const output = {
@@ -76,8 +75,8 @@ Apify.main(async () => {
         name: 'apify/igsys/linguee',
         input,
         definitions: results,
-    };
-    console.log('My output:');
-    console.dir(output);
-    await Apify.setValue('OUTPUT', output);
-});
+    }
+    console.log('My output:')
+    console.dir(output)
+    await Apify.setValue('OUTPUT', output)
+})
